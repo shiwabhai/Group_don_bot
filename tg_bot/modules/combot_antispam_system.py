a6e5e23b5cccdb3b74abf6d831ca5b283f36e8aa
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-10-21 08:39:09 +0530

    Update combot_antispam_system.py

--- tg_bot/modules/combot_antispam_system.py
+++ tg_bot/modules/combot_antispam_system.py
@@ -310,7 +310,7 @@ Commands:
  - /cas: Info about CAS. (What is CAS?)
 """
 
-__mod_name__ = "CAS"
+__mod_name__ = "CAS SYSTEM"
 
 SETCAS_HANDLER = CommandHandler("setcas", setcas, filters=Filters.group)
 GETCAS_HANDLER = CommandHandler("getcas", get_current_setting, filters=Filters.group)
